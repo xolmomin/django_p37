1698b11006995b6aa34926d551df3b9bbeb3a404
Kholmumin <xolmomin@gmail.com>
2026-03-17 21:43:53 +0500

    change

--- apps/urls.py
+++ apps/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from apps.views import product_list_page, product_detail_page
+from apps.views import product_list_page, product_detail_page, login_page, logout_page
 
 urlpatterns = [
     path('', product_list_page, name='product_list_page'),
     path('product-detail/<int:pk>', product_detail_page, name='product_detail_page'),
+    path('login', login_page, name='login_page'),
+    path('logout', logout_page, name='logout_page'),
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect
 
 from apps.models import Product
 
@@ -19,3 +22,21 @@ def product_detail_page(request, pk):
     }
 
     return render(request, 'apps/product-details.html', context)
+
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')  # 123 -> hashed_password
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('product_list_page')
+
+    return render(request, 'apps/login.html')
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('product_list_page')
